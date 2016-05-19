2d5b8f8244f6d131db95a504baf54e7b4ab5e4d1
Ray <ray@rayps.com>
2016-05-19 16:20:48 +0800

    Core functions

--- Instagrab/ViewController.swift
+++ Instagrab/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Kanna
 
 class ViewController: UIViewController {
+    
+    let myURLString: String = "https://www.instagram.com/p/BFk9vOTEcGj/"
+//    let myURLString: String = "https://www.instagram.com/p/BFkpmSIx5KT/"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        grabContent { (result) in
+            print(result)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func grabContent(completion: (result: String) -> Void) {
+        grabHTML(myURLString) { (document) in
+            
+            let video: XMLNodeSet = document.css("meta[property='og:video']")
+            let image: XMLNodeSet = document.css("meta[property='og:image']")
+            
+            var metaTag: XMLElement
+            
+            if video.count > 0 {
+                metaTag = video.first!
+            } else {
+                metaTag = image.first!
+            }
+            
+            completion(result: metaTag["content"]!)
+        }
     }
-
-
+    
+        
+    func grabHTML(url: String, completion: (document: HTMLDocument) -> Void) {
+        guard let myURL = NSURL(string: url) else {
+            print("Error: \(url) doesn't seem to be a valid URL")
+            return
+        }
+        do {
+            let myHTMLString = try String(contentsOfURL: myURL)
+            //            print(myHTMLString)
+            if let html = Kanna.HTML(html: myHTMLString, encoding: NSUTF8StringEncoding) {
+                completion(document: html)
+            }
+        }
+        catch let error as NSError {
+            print("Error: \(error)")
+        }
+        
+    }
 }
+
+
 
